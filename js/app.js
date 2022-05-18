f6a682e76086e54b06f4cc8abe203cb5a27bae66
llvovll89 <svvvs5579@naver.com>
2022-05-18 18:16:43 +0900

    js add

--- js/app.js
+++ js/app.js
@@ -1,17 +1,39 @@
 // const 변수에 , 붙이면 const 생략하고 연달아 사용가능
-const wrap = document.querySelector('.wrap'),
-Img = wrap.querySelector('.img-area img'),
-Name = wrap.querySelector('.song-details .name'),
-artist = wrap.querySelector('.song-details .artist');
+const wrap = document.querySelector(".wrap"),
+  Img = wrap.querySelector(".img-area img"),
+  Name = wrap.querySelector(".song-details .name"),
+  Artist = wrap.querySelector(".song-details .artist"),
+  Music = wrap.querySelector("#main-audio"),
+  pauseBtn = wrap.querySelector(".play-pause");
 
 let musicIndex = 2;
 
-window.addEventListener('load', () => {
-    loadMusic(musicIndex);
-})
+window.addEventListener("load", () => {
+  loadMusic(musicIndex);
+});
 
-function loadMusic() {
-
+function loadMusic(indexNumb) {
+  Name.innerText = musicAll[indexNumb - 1].name;
+  Artist.innerText = musicAll[indexNumb - 1].artist;
+  Img.src = `../img/${musicAll[indexNumb - 1].img}.jpg`;
+  Music.src = `../mp3/${musicAll[indexNumb - 1].src}.mp3`;
 }
 
+// play music
+function playMusic() {
+  wrap.classList.add("paused");
+  Music.play();
+}
 
+// pause music
+function pauseMusic() {
+    wrap.classList.remove("paused");
+    Music.pause();
+  }
+  
+
+// play btn
+pauseBtn.addEventListener("click", () => {
+  const musicPaused = wrap.classList.contains("paused");
+  musicPaused ? pauseMusic() : playMusic();
+});
--- js/list.js
+++ js/list.js
@@ -4,8 +4,38 @@ let musicAll = [
     {
         name: "멋쟁이 토마토",
         artist: "마스터 뚠식",
-        img: "",
-        src: "",
+        img: "img1",
+        src: "music1",
+    },
+    {
+        name: "애교대장 뚠식이",
+        artist: "애교왕 김뚠식",
+        img: "img2",
+        src: "music2",
+    },
+    {
+        name: "멋쟁이 뚱마토",
+        artist: "돼지 김건호",
+        img: "img3",
+        src: "music3",
+    },
+    {
+        name: "누가누가 잘하나",
+        artist: "박도꿍",
+        img: "img4",
+        src: "music4",
+    },
+    {
+        name: "노는게 제일좋아",
+        artist: "포로로",
+        img: "img5",
+        src: "music5",
+    },
+    {
+        name: "다같이 불러요~",
+        artist: "박도꿍",
+        img: "img6",
+        src: "music6",
     },
     // 음악 수 만큼 추가하기
 ]
